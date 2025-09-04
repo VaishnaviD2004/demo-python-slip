ccdcf298505f88eaeebc8d9b3e305ec657a95f7d
VaishnaviD2004 <deshmanevaishnavi254@gmail.com>
2025-09-04 10:29:16 +0530

    repetition of strings by overloading * operator.

---
+++ slip22a.py
@@ -0,0 +1,8 @@
+class Sol:
+    list_of_colors = ["Red", "Green", "White", "Red"]
+    colors = '-'.join(list_of_colors)  # *
+
+print()
+print("All Colors:", Sol.colors)   # variable class मधून access करणे
+print()
+
